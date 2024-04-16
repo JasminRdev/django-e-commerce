be7ee9d830542d1ef3125af5ecfa52a29013aa27
Jasmin <eyesthestic@gmail.com>
2024-04-16 23:44:01 +0200

    dynamic html from db, store page added

--- greatkart/views.py
+++ greatkart/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from store.models import Product
 
 def home(request):
-    products = Product.objects.all().filter(is_avaible=True)
+    products = Product.objects.all().filter(is_available=True)
 
     context = {
         'products': products,
--- store/admin.py
+++ store/admin.py
@@ -5,6 +5,6 @@ from .models import Product
 
 class ProductAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('product_name',)}
-    list_display = ('product_name', 'price', 'stock', 'category', 'modified_date', 'is_avaible')
+    list_display = ('product_name', 'price', 'stock', 'category', 'modified_date', 'is_available')
 
 admin.site.register(Product, ProductAdmin)
